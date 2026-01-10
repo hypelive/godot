c016cf23ad4746b420db14ae60655b34c4ff0106
AmtHype <nativeformea@gmail.com>
2026-01-10 18:48:05 +0100

    cmd patch

--- radio-punk.cmd
+++ radio-punk.cmd
@@ -1,4 +1,4 @@
 @echo off
 REM Launch Godot Editor with the Radio Punk project
 
-"C:\Dev\godot\bin\godot.windows.editor.x86_64.exe" -e --path "C:/Dev/radio-punk/"
+"C:\Dev\godot\bin\godot.windows.editor.x86_64.exe" --rendering-driver d3d12 -e --path "C:/Dev/radio-punk/"
---
+++ samples.cmd
@@ -0,0 +1,4 @@
+@echo off
+REM Launch Godot Editor with the Radio Punk project
+
+"C:\Dev\godot\bin\godot.windows.editor.x86_64.exe" --rendering-driver d3d12 -e --path "C:/Dev/spectrum/"
